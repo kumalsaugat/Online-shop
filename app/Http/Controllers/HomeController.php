91f63ec51f290d42c90af845fcf97e8c10ed1397
Saugat <saugatkumal452@gmail.com>
2023-09-15 18:50:56 +0545

    Authentication admin and User Complete

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class HomeController extends Controller
+{
+    public function index(){
+        $admin = Auth::guard('admin')->user();
+        echo 'welcome HOME'. $admin->name . '<a href= "'.route('admin.logout'). '">Logout</a>';
+    }
+
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ Route::get('/', function () {
 });
 
 Route::get('/admin/login',[App\Http\Controllers\AdminLoginController::class, 'index'])->name('admin.login');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::group(['middleware' => 'admin.guest'], function() {
+        Route::get('/login',[App\Http\Controllers\AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate',[App\Http\Controllers\AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+
+    });
+
+    Route::group(['middleware' => 'admin.auth'], function() {
+        Route::get('/dashboard',[App\Http\Controllers\HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout',[App\Http\Controllers\HomeController::class, 'logout'])->name('admin.logout');
+    });
+});
